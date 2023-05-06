5711386338a77bd421d48a8cab122f0f4ef6c555
Vlad Prodan <vladprodan6@gmail.com>
2023-05-06 06:05:45 +0200

    less config

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img className="App-logo" src={reactLogo} alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
